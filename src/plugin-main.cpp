83539dca2fa514c97fe3741898c7d46ac287d3e7
AllegroMoltoV <v.molto.allegro@gmail.com>
2026-01-04 15:24:35 +0900

    change c files to cpp

---
+++ src/plugin-main.cpp
@@ -0,0 +1,16 @@
+#include <obs-module.h>
+#include <plugin-support.h>
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+extern "C" bool obs_module_load(void)
+{
+	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+	return true;
+}
+
+extern "C" void obs_module_unload(void)
+{
+	obs_log(LOG_INFO, "plugin unloaded");
+}
